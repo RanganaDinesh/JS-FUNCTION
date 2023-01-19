259d675c16877ca16a1738cd11008fff2de25c38
RanganaDinesh <118040042+RanganaDinesh@users.noreply.github.com>
2023-01-19 21:16:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,103 @@
+//Create one function with zero parameter having a console statement;
+// function prepbytes() {
+//     console.log("EA19 BATCH")
+// }
+// OUTPUT : "EA19 BATCH"
+// //Create one function which takes two values as a parameter and print the sum of them as "Sum of 3, 4 is 7"
+
+// function sum(a, b) {
+//     console.log(a + b);
+// }
+// sum(3, 4);
+// OUTPUT: 7;
+// 3. questions -
+// const add = (a, b) => a + b;
+//>>>>>>>>>>.
+// This is an arrow function that takes in two parameters, a and b, and returns their sum. It is assigned to the variable add. To call this function, you would use add(1, 2) which would return 3.
+// const add = (a, b) => a + b;
+
+
+
+// var x = 21;
+// var girl = function () {
+//
+//     ;///>>>>>>>>>code will excuted line by line
+//     console.log(x);
+// var x = 20
+// }
+ 
+// girl()
+
+//>>>>>>>>>>>>
+
+
+// var x = 21;
+// girl ();// undefine
+// console.log(x)
+// function girl() {
+//     console.log(x);//// here x= 21
+//     var x = 20;
+// };
+
+
+
+// var x = 21;
+// a();
+// b();
+
+//   function a() {
+    
+//    x = 20; /// a= 20
+//   console.log(x);
+// };
+//  function b() {
+    
+//     x = 40;//b = 40
+//    console.log(x);
+// };
+
+
+//7Q.Write a function that accepts parameter n and returns factorial of n
+// const factorial = (n) => {
+//     let result = 0;
+//     for (let i = 0; i <= n; i++) {
+//       result *= i;
+//     }
+//     return result;
+// }
+
+//         var x = 21;
+//         var girl = function () {
+//             console.log(x);
+//             var x = 20;
+//         };
+// girl();
+//         OUTPUT : UNDEFINE
+ 
+// var x = 21;
+// girl ();
+// console.log(x)
+// output: undefined
+// function girl() {
+//     console.log(x);
+//     var x = 20;
+// };
+// output: 21
+// var x = 21;
+// a();
+// b();
+
+//   function a() {
+    
+//    x = 20;
+//   console.log(x);
+// };
+// output : 20
+//  function b() {
+    
+//     x = 40;
+//    console.log(x);
+// };
+// output : 40
+
+const add = (a, b) => a + b;
